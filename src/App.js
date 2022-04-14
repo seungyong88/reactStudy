9e9b81b751ebf0257873d50e8bd5dc5e65f4208d
seungyong88 <gosng2001@naver.com>
2022-04-14 13:38:46 +0900

    배열 렌더링

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Wrapper from './Wrapper';
 import Counter from "./Counter";
 import InputSample from "./InputSample";
+import UserList from "./UserList";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Wrapper>
       <Counter />
       <InputSample />
+      <UserList />
     </Wrapper>
   )
 }
